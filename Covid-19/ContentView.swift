b6d54d63f94144543b99ebe15bd48d14fd1cb58a
picachu2333 <49934825+picachu2333@users.noreply.github.com>
2020-10-09 10:15:06 +0900

    Initial Commit

---
+++ Covid-19/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Covid-19
+//
+//  Created by 이주한 on 2020/10/09.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Covid-19/Covid_19App.swift
@@ -0,0 +1,17 @@
+//
+//  Covid_19App.swift
+//  Covid-19
+//
+//  Created by 이주한 on 2020/10/09.
+//
+
+import SwiftUI
+
+@main
+struct Covid_19App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
